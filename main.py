4a4c6145150ee40c9cf5456ee28213e6bbc2ad7c
Gex-dev69 <dgexion@outlook.com>
2023-02-06 21:05:23 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import random,os
+
+random_words = ["random","letter","from","list"]
+attemp = 7
+the_word = random.sample(random_words,1)
+
+
+print(the_word[0])
+
+print(len(the_word[0]))
+
+subject = "*"*len(the_word[0])
+
+SubjectList = list(subject)
+
+def index_Finder(charc,word):
+    return [i for i, ltr in enumerate(word) if ltr in charc]
+
+
+print( "*"*len(the_word[0]))
+while True:
+    test = input("GIVE ME? ")
+    if len(test) > 1:
+        print("Please choose only one letter! ")
+        continue
+    if len(index_Finder(test,the_word[0])) <= 0:
+        attemp -= 1
+        os.system("cls")
+        print(f"Wrong!!\nYou have {attemp} attempts left")
+        print("".join(SubjectList))
+    elif attemp == 0:
+        os.system("cls")
+        print("You lost")
+        break
+    else:
+        for x in index_Finder(test,the_word[0]):
+            SubjectList[x] = test   
+        os.system("cls")
+        print("".join(SubjectList))
+    if "*" not in SubjectList:
+        print("You Won")
+        break
+    
+
+
+    
+
