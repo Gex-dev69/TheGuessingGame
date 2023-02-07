417084bcc64d2451541b65d8b5d37712705c7c42
Gex-dev69 <dgexion@outlook.com>
2023-02-07 17:28:48 +0300

    Set Homepage

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./Header.scss";
-import styled from 'styled-components';
+import temp_img from '../../assets/__Idle.gif';
+import temp_img_2 from '../../assets/playBtn.png';
+//import temp_img_2 from '../../assets/flip_skel.gif';
 
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer;
-`;
+//var ReactDOM = require('react-dom');
+var GifPlayer = require('react-gif-player');
+
+
 
 const Header = () => (
   <div className="header">
     <h2>GUESS TO LIVE</h2>
-    <Button>
-      Disabled Button
-    </Button>
+    <img src={temp_img_2} alt="look" className="sound_ico"></img>
+    <input type="text" id="playerName" className="input_prt" maxLength={10}></input>
     <button className="btn"><text className="TextStyle">PLAY</text></button>
+    <GifPlayer   gif={temp_img} still={temp_img} />
   </div>
 );
 
